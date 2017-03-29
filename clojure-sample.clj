c4796c0606c69725b4d4d703e04b6578cd2459c7
Emily Seibert <emily.seibert@willowtreeapps.com>
2017-03-29 10:45:05 -0400

    Update clojure-sample.clj

--- clojure-sample.clj
+++ clojure-sample.clj
@@ -1,15 +1,14 @@
 (fn [state time-left]
-  (def turn-directions [:right :left :about-face])
-  (def smoke-directions [:forward :backward :left :right :drop])
+  (let [move {:action :move
+              :metadata {}}
+        turn-left {:action :turn
+                   :metadata {:direction :left}}
+        turn-about {:action :turn
+                    :metadata {:direction :about-face}}
+        throw-smoke-left {:action :smoke
+                          :metadata {:direction :left}}
+        drop-smoke {:action :smoke}
+        shoot {:action :shoot}]
 
-  (let [command-options [(repeat 10 {:action :move
-                                     :metadata {}})
-                         (repeat 2 {:action :turn
-                                    :metadata {:direction (rand-nth turn-directions)}})
-                         (repeat 4 {:action :shoot
-                                      :metadata {}})
-                         (repeat 1 {:action :smoke
-                                    :metadata {:direction (rand-nth smoke-directions)}})]]
-
-    {:command (rand-nth (flatten command-options))
+    {:command shoot
      :state {}}))
